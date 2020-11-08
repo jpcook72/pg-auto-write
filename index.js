000559cb6026be38f521ec421c01b3852d89dcae
John Cook <jpcook72@gmail.com>
2020-11-08 14:42:27 -0600

    Test database creation

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-exports.printMsg = function() {
-    console.log('hey this is working')
+const Sequelize = require('sequelize');
+
+const fromGet = {
+    tables: [
+            {    
+                name: 'Food',
+                fields: [
+                    {
+                        name: 'name',
+                        type: 'string'
+                    },
+                    {
+                        name: 'price',
+                        type: 'float'
+                    },
+                ]
+            },
+            {
+                name: 'Drink',
+                fields: [
+                    {
+                        name: 'name',
+                        type: 'string'
+                    },
+                    {
+                        name: 'price',
+                        type: 'float'
+                    },
+                ]
+            }
+            ],
 }
+
+exports.makeDB = function(db) {
+
+    exportTables = {}
+
+    fromGet.tables.forEach( (table) => {
+
+        const fieldList = {}
+        console.log(table)
+
+        table.fields.forEach( (field) => {
+            fieldList[field.name] = {}
+            fieldList[field.name].type = Sequelize[field.type.toUpperCase()]
+        })
+
+
+        exportTables[table.name] = db.define(table.name.toLowerCase(), fieldList)
+
+
+    })
+    return {db, ...exportTables}
+}
+
+// const tables = makeDB(db)
+
