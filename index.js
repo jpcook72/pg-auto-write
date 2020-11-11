85628b4fcaaa03658763e77b314f99daecb2ecba
John Cook <jpcook72@gmail.com>
2020-11-11 00:32:37 -0600

    Add await

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 const axios = require('axios')
-const express = require("express")
+// const express = require("express")
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-const resp = axios.get('localhost:3035/api/schema/1')
+const resp = await axios.get('localhost:3035/api/schema/1')
 console.log(resp.data)
 const fromGet = resp.data
 
