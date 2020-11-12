c71b52e2684a6301f3bed266b3d4329b98250c05
John Cook <jpcook72@gmail.com>
2020-11-11 19:52:16 -0600

    Complete restructure

--- index.js
+++ index.js
@@ -5,31 +5,66 @@ const express = require("express")
 const app = express();
 app.use(express.json());
 
-exports.makeDB = async function(db) {
 
-    const resp = await axios.get('localhost:3035/api/schema/1')
-    const fromGet = resp.data
-    console.log('it is getting', fromGet)
+const DBsync = async function (dbName) {
 
-    exportTables = {}
+    const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+        logging: false,
+      });
 
-    fromGet.tables.forEach( (table) => {
-
-        const fieldList = {}
-        console.log(table)
-
-        table.fields.forEach( (field) => {
-            fieldList[field.name] = {}
-            fieldList[field.name].type = Sequelize[field.type.toUpperCase()]
+    const makeDB = async function(db) {
+        const resp = await axios.get('http://localhost:3035/api/schema/1')
+        const fromGet = resp.data
+    
+        exportTables = {}
+        fromGet.tables.forEach( (table) => {
+            const fieldList = {}
+            table.fields.forEach( (field) => {
+                fieldList[field.name] = {}
+                fieldList[field.name].type = Sequelize[field.type.toUpperCase()]
+            })
+            exportTables[table.name] = db.define(table.name.toLowerCase(), fieldList)
         })
 
+        return exportTables
+    }
 
-        exportTables[table.name] = db.define(table.name.toLowerCase(), fieldList)
-
-
+    const tables = await makeDB(db);
+    await db.sync({force:true})
+    module.exports['db'] = db;
+    Object.keys(tables).forEach( table => {
+        module.exports[table] = tables[table]
     })
-    return {db, ...exportTables}
 }
+
+exports.makeDB = {makeDB}
+
+
+// exports.makeDB = async function(db) {
+
+//     const resp = await axios.get('localhost:3035/api/schema/1')
+//     const fromGet = resp.data
+//     console.log('it is getting', fromGet)
+
+//     exportTables = {}
+
+//     fromGet.tables.forEach( (table) => {
+
+//         const fieldList = {}
+//         console.log(table)
+
+//         table.fields.forEach( (field) => {
+//             fieldList[field.name] = {}
+//             fieldList[field.name].type = Sequelize[field.type.toUpperCase()]
+//         })
+
+
+//         exportTables[table.name] = db.define(table.name.toLowerCase(), fieldList)
+
+
+//     })
+//     return {db, ...exportTables}
+// }
 
 // const tables = makeDB(db)
 
